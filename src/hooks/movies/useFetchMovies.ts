7b7e6edd4c0d68f3c628f233ab7fcb087662018f
Franklin Okiya <frankokiya@gmail.com>
2025-02-09 14:34:02 +0800

    chome: Enhance useFetchMovies hook with error handling - Configure iOS network security for API access - Add pagination and data deduplication

--- src/hooks/movies/useFetchMovies.ts
+++ src/hooks/movies/useFetchMovies.ts
@@ -5,14 +5,14 @@ type MovieArgs = {
   page: number;
 };
 
-const {
-  ODBM_API_KEY,
-  ODBM_API_URL,
-} = process.env;
+const {ODBM_API_KEY = 'b9bd48a6', ODBM_API_URL = 'https://www.omdbapi.com/'} =
+  process.env;
 
 export const useFetchMovies = () => {
-  const [fetchedMovies, setFetchedMovies] = useState();
+  const [fetchedData, setFetchedData] = useState<Record<string, any>[]>([]);
+  const [dataCount, setDataCount] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
   const abortControllerRef = useRef<AbortController | null>(null);
 
   const handleFetchMovies = async ({searchTerm, page}: MovieArgs) => {
@@ -20,6 +20,7 @@ export const useFetchMovies = () => {
     if (abortControllerRef.current) {
       abortControllerRef.current.abort();
     }
+    setIsLoading(true);
 
     // Create a new AbortController for the current request
     const abortController = new AbortController();
@@ -32,24 +33,80 @@ export const useFetchMovies = () => {
       s: searchTerm,
       page: page.toString(),
     });
-
+    const url = `${ODBM_API_URL}?${params.toString()}`;
     try {
-      const response = await fetch(`${ODBM_API_URL}?${params.toString()}`, {
+      const response = await fetch(url, {
         signal,
       });
       const data = await response.json();
-      setFetchedMovies(data);
-      setIsLoading(false);
-      return data;
+
+      if (data.Error) throw data;
+
+      const newData = [...fetchedData,...(data.Search ?? [])];
+      const uniqueData = Array.from(
+        new Map(newData.map(item => [item.imdbID, item])).values(),
+      );
+      setFetchedData(uniqueData);
+      setDataCount(Number(data.totalResults));
     } catch (error: unknown) {
       if (error instanceof Error && error.name === 'AbortError') {
-        console.log('Request aborted');
+        console.error('Request aborted');
       } else {
-        console.log(error);
+        console.error(error);
       }
+
+      if (typeof error === 'object' && error !== null && 'Error' in error) {
+        setError((error as {Error: string}).Error);
+        setDataCount(0);
+        setFetchedData([])
+      } else {
+        setError('An unknown error occurred');
+      }
+      
+    } finally {
       setIsLoading(false);
     }
   };
+
+  const handleFetchMovieById = async (movieId: string) => {
+    setIsLoading(true);
+    try {
+      // Create a new AbortController for the current request
+      const abortController = new AbortController();
+      const signal = abortController.signal;
+
+      abortControllerRef.current = abortController;
+
+      const params = new URLSearchParams({
+        apikey: ODBM_API_KEY!,
+        i: movieId,
+      });
+
+      const url = `${ODBM_API_URL}?${params.toString()}`;
+      const response = await fetch(url, {
+        signal,
+      });
+
+      const data = await response.json();
+      setFetchedData(data);
+    } catch (error: unknown) {
+      if (error instanceof Error && error.name === 'AbortError') {
+        console.error('Request aborted');
+      } else {
+        console.error(error);
+      }
+
+      if (typeof error === 'object' && error !== null && 'Error' in error) {
+        setError((error as {Error: string}).Error);
+      } else {
+        setError('An unknown error occurred');
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {}, []);
 
   useEffect(() => {
     return () => {
@@ -61,8 +118,11 @@ export const useFetchMovies = () => {
   }, []);
 
   return {
-    handleFetchMovies,
-    fetchedMovies,
+    dataCount,
+    fetchedData,
     isLoading,
+    error,
+    handleFetchMovies,
+    handleFetchMovieById,
   };
 };
